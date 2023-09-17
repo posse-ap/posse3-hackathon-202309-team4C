2671b2a17a8799d92f04f77c0eccc86493c93b93
ERI0914 <white.owl1031@gmail.com>
2023-09-17 22:44:56 +0900

    ヘッダー変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,25 @@
 
 //タブ　制作者:
 
-//画像の透明度を動かす　製作者:
-const checkBox = document.querySelector('input[type="checkbox"]');
-const presentImg = document.querySelector(.'present-img');
+//画像の透明度を動かす　製作者:荒谷英里
+//let obj = document.getElementsByClassName("present-img")[0];
+//    obj.style.opacity = 0.5;
 
-presentImg.addEventListener("click",function(){
 
-})
 
-function present(){
-    let obj = document.getElementsByClassName("present-img")[0];
-    obj.style.opacity = 0.5; // 透明度を50%に指定
-  }
 
-for (var i = 0; i < document.form1.elements.length - 1; i++) {
-    if (document.form1.elements[i].checked) {
-    flag = true;
-    }//trueとfaleが切り替わるらしい
-}
-if (flag = true) {present()
-    
-}
+//やり直し
+const present = document.getElementsByClassName('present-img');
+
+const btn1 = document.getElementById('box1');
+btn1.addEventListener('click',function(){
+  present.classList.add('first');
+});
 
 
 
 
 
-function butotnClick(){
-  if (box1.checked){
-    console.log('チェック');
-  } else {
-    console.log('なし');
-  }
 
 
 // submitフォーム制御　制作者:瀬戸裕文
