ee6934a2dbafa455d6f962802c67543f37dab4be
ERI0914 <white.owl1031@gmail.com>
2023-09-17 17:58:53 +0900

    マイページcss修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,38 @@
 //タブ　制作者:
 
 //画像の透明度を動かす　製作者:
+const checkBox = document.querySelector('input[type="checkbox"]');
+const presentImg = document.querySelector(.'present-img');
+
+presentImg.addEventListener("click",function(){
+
+})
+
+function present(){
+    let obj = document.getElementsByClassName("present-img")[0];
+    obj.style.opacity = 0.5; // 透明度を50%に指定
+  }
+
+for (var i = 0; i < document.form1.elements.length - 1; i++) {
+    if (document.form1.elements[i].checked) {
+    flag = true;
+    }//trueとfaleが切り替わるらしい
+}
+if (flag = true) {present()
+    
+}
+
+
+
+
+
+function butotnClick(){
+  if (box1.checked){
+    console.log('チェック');
+  } else {
+    console.log('なし');
+  }
+
 
 // submitフォーム制御　制作者:
 
